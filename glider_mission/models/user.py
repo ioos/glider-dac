ee354da1ff2c6cc5f4d76e694340249076824d6f
Dave Foster <dfoster@asascience.com>
2013-08-13 13:35:18 -0400

    Mission timestamp/file count

--- glider_mission/models/user.py
+++ glider_mission/models/user.py
@@ -1,9 +1,11 @@
-from flask_login import UserMixin
 import os
 import os.path
+import glob
 import sys
+from datetime import datetime
 from pam import authenticate
 from glider_mission import app
+from flask_login import UserMixin
 
 class User(UserMixin):
     def __init__(self, id):
@@ -34,12 +36,20 @@ class User(UserMixin):
         retval = []
 
         for mission in missions:
+            data_files = glob.glob(os.path.join(self.data_root, mission, "*.nc"))
+            datacount = len(data_files)
+
+            last_updated = None
+            stamps = sorted([os.stat(x).st_mtime for x in data_files])
+            if len(stamps):
+                last_updated = datetime.fromtimestamp(stamps[-1])
+
             wmo_id_file = os.path.join(self.data_root, mission, "wmoid.txt")
             if os.path.exists(wmo_id_file):
                 with open(wmo_id_file) as f:
-                    retval.append((mission, f.read().strip()))
+                    retval.append((mission, f.read().strip(), datacount, last_updated))
             else:
-                retval.append((mission, None))
+                retval.append((mission, None, datacount, last_updated))
 
         return retval
 
