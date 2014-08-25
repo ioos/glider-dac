e873c2aab816c13121439287fe0bf457564e0ce6
Dave Foster <dfoster@asascience.com>
2014-08-25 16:53:52 -0400

    Add agg support for ERDDAP catalogs (for public only)

--- scripts/build_erddap_catalog.py
+++ scripts/build_erddap_catalog.py
@@ -8,6 +8,7 @@ import fileinput
 import glob
 from string import Template
 from lxml import etree
+from collections import defaultdict
 
 logging.basicConfig(level=logging.INFO,
                     format='[%(asctime)s | %(levelname)s]  %(message)s')
@@ -27,17 +28,28 @@ def build_erddap_catalog(data_root, catalog_root, erddap_name, template_dir):
     tail_path = os.path.join(template_dir, 'datasets.tail.xml')
 
     # discover all deployments in data directory
-    pattern = os.path.join(data_root, '*', '*')
+    udeployments    = defaultdict(list)
+    pattern         = os.path.join(data_root, '*', '*')
     deployment_dirs = glob.glob(pattern)
+
+    for dd in deployment_dirs:
+        user, deployment = os.path.split(os.path.relpath(dd, data_root))
+        udeployments[user].append(deployment)
 
     ds_path = os.path.join(catalog_root, erddap_name, 'datasets.xml')
     with open(ds_path, 'w') as f:
         for line in fileinput.input([head_path]):
             f.write(line)
 
-        for dd in deployment_dirs:
-            user, deployment = os.path.split(os.path.relpath(dd, data_root))
-            f.write(build_erddap_catalog_fragment(data_root, user, deployment, template_dir))
+        # for each user deployment, create a dataset fragment
+        for user in udeployments:
+            for deployment in udeployments[user]:
+                f.write(build_erddap_catalog_fragment(data_root, user, deployment, template_dir))
+
+        # if we have an "agg" file in our templates, fill one out per user
+        if os.path.exists(os.path.join(template_dir, 'dataset.agg.xml')):
+            for user in udeployments:
+                f.write(build_erddap_agg_fragment(data_root, user, template_dir))
 
         for line in fileinput.input([tail_path]):
             f.write(line)
@@ -68,9 +80,44 @@ def build_erddap_catalog_fragment(data_root, user, deployment, template_dir):
     except (OSError, IOError, AssertionError, AttributeError):
         pass
 
-    return template.substitute(dataset_id=deployment,
-                               dataset_dir=dir_path,
-                               institution=institution)
+    return template.safe_substitute(dataset_id=deployment,
+                                    dataset_dir=dir_path,
+                                    institution=institution)
+
+def build_erddap_agg_fragment(data_root, user, template_dir):
+    """
+    Builds an aggregate dataset fragment entry.
+
+    For Glider DAC this is on the public ERDDAP instance.
+    """
+    logger.info("Building ERDDAP catalog aggregation fragment for %s", user)
+
+    # grab template for dataset fragment
+    template_path = os.path.join(template_dir, 'dataset.agg.xml')
+    with open(template_path) as f:
+        template = Template("".join(f.readlines()))
+
+    institution     = user
+    dir_path        = os.path.join(data_root, user)
+
+    # grab institution, if we can find from first deployment
+    pattern     = os.path.join(data_root, user, "*", "deployment.json")
+    deployments = glob.glob(pattern)
+
+    if len(deployments):
+        try:
+            with open(deployments[0]) as f:
+                js              = json.load(f)
+                institution     = js.get('operator', js.get('username'))
+        except (OSError, IOError, AssertionError, AttributeError):
+            pass
+
+    dataset_title   = "All %s Gliders" % institution
+    dataset_id      = "all%sGliders" % slugify(institution)
+
+    return template.safe_substitute(dataset_id=dataset_id,
+                                    dataset_dir=dir_path,
+                                    dataset_title=dataset_title)
 
 def make_all_dirs(catalog_root, mode):
     """
@@ -84,6 +131,18 @@ def make_all_dirs(catalog_root, mode):
             os.makedirs(d)
         except OSError:
             pass
+
+def slugify(value):
+    """
+    Normalizes string, removes non-alpha characters, and converts spaces to hyphens.
+    Pulled from Django
+    """
+    import unicodedata
+    import re
+    value = unicode(value)
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+    value = unicode(re.sub('[^\w\s-]', '', value).strip())
+    return unicode(re.sub('[-\s]+', '-', value))
 
 def main(mode, data_root, catalog_root, templates):
 
