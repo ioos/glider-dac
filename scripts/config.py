64e4bfe0405df26814b6a6f23b487054c370de6b
Luke Campbell <luke.s.campbell@gmail.com>
2014-10-27 13:26:34 -0400

    Adds back in config.py

---
+++ scripts/config.py
@@ -0,0 +1,6 @@
+# Configure Variables
+JSON_DIR = '/data/data/priv_erddap/'
+path2priv='/data/data/priv_erddap/'
+path2pub='/data/data/pub_erddap/'
+path2thredds = '/data/data/thredds/'
+SERVER="http://localhost:8080/erddap"
