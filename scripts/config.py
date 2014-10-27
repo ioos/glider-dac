5f7149f205a74724204a53a981f6a10a978d836c
Luke Campbell <luke.s.campbell@gmail.com>
2014-10-27 13:14:44 -0400

    Removes config.py error.log from git

--- scripts/config.py
+++
@@ -1,6 +0,0 @@
-# Configure Variables
-JSON_DIR = '/data/data/priv_erddap/'
-path2priv='/data/data/priv_erddap/'
-path2pub='/data/data/pub_erddap/'
-path2thredds = '/data/data/thredds/'
-SERVER="http://localhost:8080/erddap"
