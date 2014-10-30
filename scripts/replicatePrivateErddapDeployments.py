8a9b5d12e2b0823b361ffb3a2e2476821d6ded98
Luke Campbell <luke.s.campbell@gmail.com>
2014-10-30 16:39:02 -0400

    Major updates to replication

--- scripts/replicatePrivateErddapDeployments.py
+++ scripts/replicatePrivateErddapDeployments.py
@@ -4,84 +4,170 @@ import json
 import os
 import sys
 import urllib2
-import feedparser
+import argparse
 import calendar
 import time
 import sh
 import glob
 import datetime
+import logging
+import requests
 
 from config import *
+log = None
 
+def poll_erddap(deployment_name, host):
+    args = {}
+    args['host'] = host
+    args['deployment_name'] = deployment_name
+    template = 'http://%(host)s/erddap/tabledap/%(deployment_name)s.das' % args
+    log.info("Polling %s", template)
+    status = request_poll(url)
+    return status
+
+def request_poll(url, attempts=15):
+    '''
+    Polls a URL for the number of specified attempts
+    returns True if the URL succeeded or False if it did not
+    '''
+    while attempts > 0:
+        response = requests.get(url)
+        if response.status_code != 200:
+            log.warning("Failed to find deployment dataset")
+        else:
+            break
+        log.info("sleeping 1 second")
+        time.sleep(1)
+        attempts -= 1
+    else:
+        return False
+    return True
+
+
+
+def touch_erddap(deployment_name, path):
+    '''
+    Creates a flag file for erddap's file monitoring thread so that it reloads
+    the dataset
+
+    path should be from config either flags_private or flags_public
+    '''
+    full_path = os.path.join(path, deployment_name)
+    log.info("Touching flag file at %s", full_path)
+    with open(full_path, 'w') as f:
+        pass # Causes file creation
+
+
+def setup_logging(level=logging.DEBUG):
+    import logging
+    logger = logging.getLogger('replicate')
+    logger.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler('/home/glider/logs/replciate.log')
+    stream_handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    file_handler.setFormatter(formatter)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(file_handler) 
+    logger.addHandler(stream_handler)
+    logger.setLevel(logging.DEBUG)
+    return logger
 
 def get_mod_time(name):
 
     jsonFile = os.path.join(JSON_DIR, name + '/deployment.json')
-    print "Inspecting", jsonFile
+    log.info("Inspecting %s", jsonFile)
 
     if not os.path.exists(jsonFile):
-        print "JSON file does not exist."
+        log.info( "JSON file does not exist.")
         newest = max(glob.iglob(JSON_DIR+name+'/'+'*.nc') , key=os.path.getmtime)
         jsonTime = os.path.getmtime(newest)
         with  open(jsonFile, 'w') as outfile:
             json.dump({'updated':jsonTime*1000}, outfile)
-        print "Initiated JSON file"
+        log.info("Initiated JSON file")
         
 
     with open(jsonFile, 'r') as fid:
         dataset = json.load(fid)
 
-    print dataset['updated']
+    log.info( dataset['updated'])
     return (dataset['updated'])/1000
 
-def main():
+def main(args):
+    global log
+    if log is None:
+        log = setup_logging()
     deployments = []
-    for u in os.listdir(path2priv):
-        if os.path.isdir(path2priv+u):
-            deployments.extend( [u+'/'+d for d in os.listdir(path2priv+u+'/')])
-    print "Processing the following deployments"
-    for d in deployments:
-        print " -", d
-    for d in deployments:
-        sync_deployment(d)
+    for user in os.listdir(path2priv):
+        if not os.path.isdir(os.path.join(path2priv, user)):
+            continue
+        for deployment_name in os.listdir(os.path.join(path2priv, user)):
+            deployment_path = os.path.join(path2priv, user, deployment_name)
+            if os.path.isdir(deployment_path):
 
-def retrieve_data(where, d):
-    ud=where+d
-    pathDeployName = ud.split('/')[-1]
-    path_arg = ud + "/" + pathDeployName + ".nc3.nc"
-    host_arg = SERVER+"/tabledap/" + pathDeployName + ".ncCFMA"
-    print "Path Arg", path_arg
-    print "Host Arg", host_arg
+                deployments.append(os.path.join(user, deployment_name))
+            
+    log.info( "Processing the following deployments")
+    for deployment in deployments:
+        log.info( " - %s", d)
+    for deployment in deployments:
+        # First sync up the private
+        touch_erddap(deployment, flags_private)
+        if not poll_erddap(deployment, erddap_private):
+            log.error("Couldn't update deployment %s", deployment)
+            continue
+        
+        retrieve_data(path2pub, deployment)
+        retrieve_data(path2thredds, deployment)
+
+        touch_erddap(deployment, flags_public)
+        if not poll_erddap(deployment, erddap_public):
+            log.error("Couldn't update public erddap for deployment %s", deployment)
+
+
+
+def retrieve_data(where, deployment):
+    publish_dir = os.path.join(where, deployment)
+    log.info("Publish Directory: %s", publish_dir)
+    deployment_name = publish_dir.split('/')[-1]
+    user_name = publish_dir.split('/')[-2]
+    path_arg = os.path.join(publish_dir, deployment_name + ".ncCF.nc3.nc")
+    host_arg = SERVER + "/tabledap/" + deployment_name + ".ncCFMA"
+    log.info( "Path Arg %s", path_arg)
+    log.info( "Host Arg %s", host_arg)
     args = [
         "--no-host-directories",
         "--cut-dirs=2",
         "--output-document=%s" % path_arg,
         host_arg
      ]
-    print "Args:", ' '.join(args)
+    log.info( "Args: %s", ' '.join(args))
     sh.wget(*args)
 
 
-def sync_deployment(deployment):
-    print "--------------------------------------------------------------------------------"
-    print "   Processing", deployment
-    print "--------------------------------------------------------------------------------"
+def sync_deployment(deployment, force=False):
+    log.info( "--------------------------------------------------------------------------------")
+    log.info( "   Processing %s", deployment)
+    log.info( "--------------------------------------------------------------------------------")
     d = deployment
     #Get Current Epoch Time and how far back in time to search
     currentEpoch = time.time()
     time_in_past = 3800
     mTime=get_mod_time(d)
     deltaT= int(currentEpoch) - int(mTime)
-    print "Synchronizing at ", datetime.datetime.utcnow().isoformat()
+    log.info( "Synchronizing at %s", datetime.datetime.utcnow().isoformat())
     #print currentEpoch, deltaT, mTime
-    if deltaT <  time_in_past: 
+    if force or deltaT <  time_in_past: 
         #Retrieve Data and send to public
+        log.error("Path to public: %s", path2pub)
         retrieve_data(path2pub, d)
         #Retrieve Data and send to Thredds
         retrieve_data(path2thredds, d)
     else:
-        print "Everything is up to date"
+        log.info("Everything is up to date")
   
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="replicate files from private ERDDAP")
+    parser.add_argument('-f', '--force', action="store_true")
+    args = parser.parse_args()
+    main(args)
 
