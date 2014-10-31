0c247c347fcd61fe2157423d8b76b3304bf823d5
Luke Campbell <luke.s.campbell@gmail.com>
2014-10-31 09:44:55 -0400

    Many more changes

--- glider_dac/defaults.py
+++ glider_dac/defaults.py
@@ -26,10 +26,11 @@ MONGODB_PASSWORD = url.password
 MONGODB_DATABASE = url.path[1:]
 
 # email
+MAIL_ENABLED        = os.environ.get('MAIL_ENABLED', None) == "True"
 MAIL_SERVER         = os.environ.get('MAIL_SERVER')
 MAIL_PORT           = os.environ.get('MAIL_PORT')
-MAIL_USE_TLS        = bool(os.environ.get("MAIL_USE_TLS", True))
-MAIL_USE_SSL        = bool(os.environ.get("MAIL_USE_SSL", False))
+MAIL_USE_TLS        = os.environ.get("MAIL_USE_TLS", True) == "True"
+MAIL_USE_SSL        = os.environ.get("MAIL_USE_SSL", False) == "True"
 MAIL_USERNAME       = os.environ.get('MAIL_USERNAME')
 MAIL_PASSWORD       = os.environ.get('MAIL_PASSWORD')
 
@@ -38,4 +39,5 @@ MAIL_DEFAULT_TO     = os.environ.get('MAIL_DEFAULT_TO')
 MAIL_DEFAULT_LIST   = os.environ.get('MAIL_DEFAULT_LIST', None)
 
 
-
+print "Mail Enabled:", MAIL_ENABLED
+print "Environ:", os.environ.get('MAIL_ENABLED')
