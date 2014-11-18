cf8c8dfcc0cb0a93f11198e5ad99d1cfdeb4b986
Luke Campbell <luke.s.campbell@gmail.com>
2014-11-18 08:23:40 -0500

    Updates WAF support to include THREDDS

--- scripts/download_waf.py
+++ scripts/download_waf.py
@@ -11,9 +11,31 @@ static directory.
 import requests
 import argparse
 import os
+import sys
 
+from thredds_crawler.crawl import Crawl
 
-def main(url, destination_path):
+def main(args):
+    if args.erddap:
+        get_erddap_waf(args.erddap, args.destination)
+    if args.thredds:
+        get_thredds_waf(args.thredds, args.destination)
+
+def get_thredds_waf(url, destination_path):
+    '''
+    Scrapes the available ISO files at the specified THREDDS instance.
+    The URL must point to the catalog.xml
+    '''
+    c = Crawl(url)
+    datasets = c.datasets
+    for dataset in datasets:
+        services = { row['name'] : row for row in dataset.services }
+        iso_url = services['iso']['url']
+        if iso_url:
+            file_path = get_iso_doc(iso_url, destination_path, dataset.id + '.xml')
+            print "Created ISO at", file_path
+
+def get_erddap_waf(url, destination_path):
     '''
     Scrapes the available datasets from ERDDAP and harvests the ISO 19115
     documents that are available. This script currently does not gather
@@ -62,7 +84,7 @@ def create_dataset_doc(doc):
 
     return datasets
 
-def get_iso_doc(iso_url, destination_path):
+def get_iso_doc(iso_url, destination_path, file_name=None):
     '''
     Downloads an ISO document at the URL specified into the destination
     '''
@@ -71,7 +93,7 @@ def get_iso_doc(iso_url, destination_path):
         raise IOError("Failed to retrieve ISO 19115 Document from ERDDAP at %s with error code %s" % (iso_url, response.status_code) )
 
     xml_doc = response.text
-    file_name = iso_url.split('/')[-1]
+    file_name = file_name or iso_url.split('/')[-1]
 
     file_path = os.path.join(destination_path, file_name)
     with open(file_path, 'wb') as f:
@@ -86,10 +108,12 @@ def get_iso_doc(iso_url, destination_path):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Download ISO documents from ERDDAP instance')
-    parser.add_argument('erddap', help='URL to ERDDAP, example: http://coastwatch.pfeg.noaa.gov/erddap')
     parser.add_argument('destination', help='Folder to download the ISO documents into')
+    parser.add_argument('-e', '--erddap', help='URL to ERDDAP, example: http://coastwatch.pfeg.noaa.gov/erddap')
+    parser.add_argument('-t', '--thredds', help='URL to THREDDS catalog.xml, example: http://data.ioos.us/gliders/thredds/catalog.xml')
     args = parser.parse_args()
-    main(args.erddap, args.destination)
+
+    main(args)
 
 
 
