dbfe5079db80fbab283c1056e170fc36c53f1c91
Luke Campbell <luke.s.campbell@gmail.com>
2017-02-17 17:06:54 -0500

    Functional integration

--- glider_dac/views/institution.py
+++ glider_dac/views/institution.py
@@ -1,23 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import render_template, redirect, flash, url_for, jsonify
-from flask_login import login_required, current_user
-from glider_dac import app, db
-
 '''
 glider_dac/views/institution.py
 View definition for institutions
 '''
+from __future__ import unicode_literals
+from flask import render_template, redirect, flash, url_for, jsonify
+from flask_login import login_required, current_user
+from glider_dac import app, db
+from flask_wtf import Form
+from wtforms import TextField, SubmitField
 
 
-@app.route('/institutions/', methods=['GET'])
+class NewInstitutionForm(Form):
+    name = TextField('Institution Name')
+    submit = SubmitField('New Institution')
+
+
+@app.route('/institutions/', methods=['GET', 'POST'])
 @login_required
 def show_institutions():
     if not current_user.is_admin():
         flash("Permission denied", 'danger')
         return redirect(url_for('index'))
-    institutions = list(db.Institutions.find())
+    institutions = list(db.Institution.find())
+    form = NewInstitutionForm()
+    if form.validate_on_submit():
+        institution = db.Institution()
+        institution.name = form.name.data
+        institution.save()
+        flash('Institution Created', 'success')
+
     return render_template('institutions.html',
+                           form=form,
                            institutions=institutions)
 
 
