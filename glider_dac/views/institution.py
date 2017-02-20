c865ecf2a00246a47eb03e7dd2dae46a6b112605
Luke Campbell <luke.s.campbell@gmail.com>
2017-02-20 12:50:56 -0500

    Adds post and delete

--- glider_dac/views/institution.py
+++ glider_dac/views/institution.py
@@ -5,11 +5,43 @@ glider_dac/views/institution.py
 View definition for institutions
 '''
 from __future__ import unicode_literals
-from flask import render_template, redirect, flash, url_for, jsonify
-from flask_login import login_required, current_user
+from flask import render_template, redirect, flash, url_for, jsonify, request
+from flask_login import current_user
 from glider_dac import app, db
 from flask_wtf import Form
 from wtforms import TextField, SubmitField
+from functools import wraps
+import json
+
+
+def error_wrapper(func):
+    '''
+    Function wrapper to catch exceptions and return them as jsonified errors
+    '''
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            return jsonify(error=type(e).__name__, message=e.message), 500
+    return wrapper
+
+
+def admin_required(func):
+    '''
+    Wraps a route to require an administrator
+    '''
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if app.login_manager._login_disabled:
+            return func(*args, **kwargs)
+        elif not current_user.is_authenticated:
+            return app.login_manager.unauthorized()
+        elif not current_user.is_admin():
+            flash("Permission denied", 'danger')
+            return redirect(url_for('index'))
+        return func(*args, **kwargs)
+    return wrapper
 
 
 class NewInstitutionForm(Form):
@@ -18,11 +50,8 @@ class NewInstitutionForm(Form):
 
 
 @app.route('/institutions/', methods=['GET', 'POST'])
-@login_required
+@admin_required
 def show_institutions():
-    if not current_user.is_admin():
-        flash("Permission denied", 'danger')
-        return redirect(url_for('index'))
     institutions = list(db.Institution.find())
     form = NewInstitutionForm()
     if form.validate_on_submit():
@@ -39,5 +68,32 @@ def show_institutions():
 @app.route('/api/institution', methods=['GET'])
 def get_institutions():
     institutions = [inst.to_json() for inst in db.Institution.find()]
-
     return jsonify(results=institutions)
+
+
+@app.route('/api/institution', methods=['POST'])
+@admin_required
+@error_wrapper
+def new_institution():
+    app.logger.info(request.data)
+    data = json.loads(request.data)
+    institution = db.Institution()
+    institution.name = data['name']
+    institution.save()
+    return institution.to_json()
+
+
+@app.route('/api/institution/<ObjectId:institution_id>', methods=['DELETE'])
+@admin_required
+@error_wrapper
+def delete_institution(institution_id):
+    if not current_user.is_admin():
+        flash("Permission denied", 'danger')
+        return redirect(url_for('index'))
+    institution = db.Institution.find_one({"_id": institution_id})
+    if institution is None:
+        return jsonify({}), 404
+    app.logger.info("Deleting institution")
+    institution.delete()
+    return jsonify({}), 204
+
