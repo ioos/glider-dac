87f59203621c429fbdc9511972e970c4b08209e0
Benjamin Adams <ben.adams@rpsgroup.com>
2018-08-23 18:37:15 +0000

    Updates watcher logic
    Updates filesystem watcher logic to properly sync/update
    necessary files/models when a file is rsynced.

--- glider_dac_db_sync.py
+++ glider_dac_db_sync.py
@@ -10,7 +10,8 @@ import subprocess
 
 from datetime import datetime
 
-from watchdog.events import FileSystemEventHandler, DirCreatedEvent, DirDeletedEvent, FileCreatedEvent
+from watchdog.events import (FileSystemEventHandler, DirCreatedEvent,
+                             DirDeletedEvent, FileCreatedEvent, FileMovedEvent)
 from watchdog.observers import Observer
 
 from glider_dac import app, db
@@ -22,6 +23,49 @@ logger = logging.getLogger(__name__)
 class HandleDeploymentDB(FileSystemEventHandler):
     def __init__(self, base):
         self.base     = base
+
+    def file_moved_or_created(self, event):
+        logger.info('%s %s', self.base, event.src_path)
+        if self.base not in event.src_path:
+            return
+
+        rel_path = os.path.relpath(event.src_path, self.base)
+        path_parts = os.path.split(rel_path)
+        logger.info("%s %s", type(event), path_parts)
+
+        # ignore if a dotfile
+        if path_parts[1].startswith('.'):
+            return
+
+
+        with app.app_context():
+            deployment = db.Deployment.find_one({'deployment_dir' : path_parts[0]})
+            if deployment is None:
+                logger.error("Cannot find deployment for %s", path_parts[0])
+                return
+
+            if path_parts[-1] == "wmoid.txt":
+                rel_path = os.path.relpath(event.src_path, self.base)
+                logger.info("New wmoid.txt in %s", rel_path)
+                if deployment.wmo_id:
+                    logger.info("Deployment already has wmoid %s.  Updating value with new file.", deployment.wmo_id)
+                with open(event.src_path) as wf:
+                    deployment.wmo_id = unicode(wf.readline().strip())
+                deployment.save()
+                logger.info("Updated deployment %s", path_parts[0])
+            else:
+                # Always save the Deployment when a new dive file is added
+                # so a checksum is calculated and a new deployment.json file
+                # is created
+                fname, ext = os.path.splitext(path_parts[-1])
+                if ext not in ['.md5', '.txt']:
+                    deployment.save()
+                    logger.info("Updated deployment %s", path_parts[0])
+
+    def on_moved(self, event):
+        if isinstance(event, FileMovedEvent):
+            self.file_moved_or_created(event)
+
 
     def on_created(self, event):
         if isinstance(event, DirCreatedEvent):
@@ -54,32 +98,7 @@ class HandleDeploymentDB(FileSystemEventHandler):
                         deployment.save()
 
         elif isinstance(event, FileCreatedEvent):
-            if self.base not in event.src_path:
-                return
-
-            path_parts = os.path.split(event.src_path)
-
-            with app.app_context():
-                deployment = db.Deployment.find_one({'deployment_dir' : path_parts[0]})
-                if deployment is None:
-                    logger.error("Cannot find deployment for %s", path_parts[0])
-                    return
-
-                if path_parts[-1] == "wmoid.txt":
-                    rel_path = os.path.relpath(event.src_path, self.base)
-                    logger.info("New wmoid.txt in %s", rel_path)
-                    if deployment.wmo_id:
-                        logger.info("Deployment already has wmoid %s.  Updating value with new file.", deployment.wmo_id)
-                    with open(event.src_path) as wf:
-                        deployment.wmo_id = unicode(wf.readline().strip())
-                    deployment.save()
-                else:
-                    # Always save the Deployment when a new dive file is added
-                    # so a checksum is calculated and a new deployment.json file
-                    # is created
-                    fname, ext = os.path.splitext(path_parts[-1])
-                    if ext not in ['.md5', '.txt']:
-                        deployment.save()
+            self.file_moved_or_created(event)
 
     def on_deleted(self, event):
         if isinstance(event, DirDeletedEvent):
