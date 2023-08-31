bdc2e750fe3ca61cb0aeed9e917abd16649301b4
Benjamin Adams <ben.adams@rpsgroup.com>
2023-08-31 15:03:58 -0400

    Create NAVOCEANO symlinking shell script
    Creates a Bash shell script intended to symlink any missing files into
    the appropriate created deployment directories for NAVOCEANO.  Intended
    to be run periodically, e.g. through crontab.

---
+++ scripts/create_navoceano_symlinks.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Make symlinks for created navo deployment directories which might
+# not have symlinks yet.
+shopt -s nullglob
+navo_dir=/data/data/priv_erddap/navoceano
+cd "$navo_dir"
+
+for dest_dir in ng*; do
+    glider_name="${dest_dir%%-*}"
+    for source_file in "hurricanes-20230601T0000/$glider_name"*.nc; do
+        no_dir="${source_file#*/}"
+        no_z="${no_dir/%Z.nc/.nc}"
+        symlink_dest="$dest_dir/${no_z/_/-}"
+        if [[ ! -L "$symlink_dest" ]]; then
+            ln -s "$navo_dir/$source_file" "$symlink_dest"
+        fi
+    done
+done
